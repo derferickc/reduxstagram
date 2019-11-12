9cbdee31f3e0c3e1354a0bc487be7d3fd9dc1cb4
Frederick Choe <frederick@glamhive.com>
2019-11-12 00:56:19 -0800

    comments add updated for comment name displaying

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -3,14 +3,16 @@
 // 2. a copy of current state
 
 function postComments(state = [], action) {
-	switch(action.type) {
+	switch(action.type){
 		case 'ADD_COMMENT':
-			return[...state, {
-				user: action.author,
-				text: action.coment
-			}]
+		// return the new state with the new comment
+		return [...state,{
+			user: action.author,
+			text: action.comment
+		}];
+	
 		case 'REMOVE_COMMENT':
-			// we need to return the new state without the deleted comment
+		// we need to return the new state without the deleted comment
 			return [
 				// from the start to the one we want to delete
 				...state.slice(0,action.i),
